8d68e6e1739fe8ed2ef5e1628e9cd7146681f6c4
Ben Ng <ben@plumcube.com>
2020-04-23 13:15:43 -0400

    update package

---
+++ build.js
@@ -0,0 +1,43 @@
+'use strict'
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+
+const data = {
+  name: chalk.white('              Ben Ng'),
+  handle: chalk.white('plumcube ') + chalk.red('⬢'),
+  work: chalk.white('Director of Engineering @ BeeswaxIO Inc.'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('plumcube'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~plumcube'),
+  github: chalk.gray('https://github.com/') + chalk.green('plumcube'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.cyan('nycben'),
+  web: chalk.cyan('https://plumcube.com'),
+  npx: chalk.green('npx') + ' ' + chalk.white('@plumcube/card'),
+  labelWork: chalk.white.bold('       Work:'),
+  labelTwitter: chalk.white.bold('    Twitter:'),
+  labelnpm: chalk.white.bold('        npm:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelWeb: chalk.white.bold('        Web:'),
+  labelCard: chalk.white.bold('       Card:')
+}
+
+const output = `${data.name} / ${data.handle}
+
+${data.labelWork}  ${data.work}
+${data.labelTwitter}  ${data.twitter}
+${data.labelnpm}  ${data.npm}
+${data.labelGitHub}  ${data.github}
+${data.labelLinkedIn}  ${data.linkedin}
+${data.labelWeb}  ${data.web}
+
+${data.labelCard}  ${data.npx}`;
+
+fs.writeFileSync(path.join(__dirname, 'bin/output'),
+  chalk.green(boxen(output, {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round'
+  }))
+);
